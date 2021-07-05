ff44435e081f5cb27efc2abbca93c335ee14f888
Dan McFarlane <danm@mcfarlaneaviation.com>
2021-07-05 14:07:38 -0500

    docs: fix formatting

--- src/motor.ads
+++ src/motor.ads
@@ -5,8 +5,8 @@ with STM32.Device;
 -------------------------------------------------------------------------------
 -- Motor
 --
--- Defines the Motor abstract data type and subprograms to operate on it.
--- Timer 3 channel 1 gets mapped to PB4 (alternate function 2)
+-- Defines the Motor abstract data type and subprograms to operate on it. Timer
+-- 3 channel 1 gets mapped to PB4 (alternate function 2)
 -------------------------------------------------------------------------------
 package Motor is
    use type STM32.GPIO.GPIO_Point;
@@ -20,19 +20,18 @@ package Motor is
    -- Configure
    --
    -- Purpose:
-   --    Configure the Motor to use the specified GPIO_Point.
-   --
+   --    Configure the GPIO pin to use for the motor PWM.
    ----------------------------------------------------------------------------
    procedure Configure
      (This : in out Motor; PWM_GPIO_Point : STM32.GPIO.GPIO_Point) with
       Pre => PWM_GPIO_Point = STM32.Device.PB4 or
       PWM_GPIO_Point = STM32.Device.PA15;
 
-   ----------------------------------------------------------------------------
-      -- Set
-      --
-   -- Set the motor rotation speed with Setpoint (-1.0) being full reverse and
-   -- Setpoint (1.0) being full forward.
+   ---------------------------------------------------------------------------
+   -- Set
+   --
+   -- Set the motor rotation speed with Setpoint (-1.0) being full reverse
+   -- and Setpoint (1.0) being full forward.
    ----------------------------------------------------------------------------
    procedure Set (This : in out Motor; New_Setpoint : Setpoint);
 
